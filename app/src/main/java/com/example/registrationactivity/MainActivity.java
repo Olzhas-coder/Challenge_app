fbe4aa587b1c627063c55b110e8bb7b4f91cb4e8
Olzhas Karibayev <olji20020000@gmail.com>
2020-12-24 10:13:04 +0600

    'LoginBySql'

---
+++ app/src/main/java/com/example/registrationactivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.registrationactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password, repassword;
+    Button signup, signin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.username)
+        password = (EditText) findViewById(R.id.password)
+        repassword = (EditText) findViewById(R.id.repassword)
+        signup = (Button) findViewById(R.id.btnsignup)
+        signin = (Button) findViewById(R.id.btnsignin)
+
+        signup.setOnClickListener(new View.OnClickListener(){
+            
+        })
+    }
+}
